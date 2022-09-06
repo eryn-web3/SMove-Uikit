bcba2e924d3260bb460970cef30b2afd3195e2ed
Sun Dong <57502391+sundong-exideatech@users.noreply.github.com>
2022-09-06 21:08:07 +0800

    Initial deployment

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# SMove-Uikit
+# 🥞 SMove UIkit
+
+[![Version](https://img.shields.io/npm/v/@smove/uikit)](https://www.npmjs.com/package/@smove/uikit) [![Size](https://img.shields.io/bundlephobia/min/@smove/uikit)](https://www.npmjs.com/package/@smove/uikit)
+
+SMove UIkit is a set of React components and hooks used to build pages on SMove's apps. It also contains a theme file for dark and light mode.
+
+## Install
+
+`yarn add @smove/uikit`
+
+## Setup
+
+### Theme
+
+Before using SMove UIkit, you need to provide the theme file to styled-component.
+
+```
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@smove/uikit'
+...
+<ThemeProvider theme={isDark}>...</ThemeProvider>
+```
+
+### Reset
+
+A reset CSS is available as a global styled component.
+
+```
+import { ResetCSS } from '@smove/uikit'
+...
+<ResetCSS />
+```
---
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { PancakeRoundIcon } from "../../../components/Svg";
+import Text from "../../../components/Text/Text";
+import Skeleton from "../../../components/Skeleton/Skeleton";
+
+interface Props {
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.a`
+  display: flex;
+  align-items: center;
+  svg {
+    transition: transform 0.3s;
+  }
+  :hover {
+    svg {
+      transform: scale(1.2);
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    border-radius: 10px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+
+    padding: 0 15px;
+    background-color: ${({ theme }) => theme.colors.priceBackground};
+    color: ${({ theme }) => theme.colors.background};
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
+  return cakePriceUsd ? (
+    <PriceLink>
+      <PancakeRoundIcon width="24px" mr="8px" />
+      <Text color="text" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
